0a22dc93d3a6dae06c2daa121f48ccdee145d26e
Oleg Degterov <olegdegterov@gmail.com>
2024-08-06 21:31:23 +0300

    features: add user router

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 import express from "express";
+import { userRouter } from "./users/users.js";
+import helmet from "helmet";
 
 const port = 8000;
 const app = express();
 
 app.get("/hello", (req, res) => {
-  // res.cookie("token", "token123456", {
-  //   domain: "",
-  //   path: "/",
-  //   secure: true,
-  //   expires: 600000,
-  // });
-  // res.clearCookie("token");
-  // res.send("{ a: 1 }");
-  // if you no logic in cb function
-  // you must to call end() method
-  res.end();
+  res.send("Hello");
 });
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"], // Allow resources from the same origin
+      connectSrc: ["'self'", "http://localhost:8000"], // Allow connections to your API
+      // Add other directives as needed
+    },
+  })
+);
+
+app.use("/users", userRouter);
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
