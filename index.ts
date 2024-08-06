1e0e54b853b5caf8c5cccacd1e38dbe4b3cd5b5b
Oleg Degterov <olegdegterov@gmail.com>
2024-08-06 23:15:44 +0300

    features: add typescript

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { userRouter } from "./users/users.js";
 // import helmet from "helmet";
 
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 app.use("/users", userRouter);
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log(err.message);
   res.status(401).send(err.message);
 });
