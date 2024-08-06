e8935369c4bd39aebcf30a0077c0ef7b5e7a0d10
Oleg Degterov <olegdegterov@gmail.com>
2024-08-06 22:41:36 +0300

    features: add middlewares

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 import express from "express";
 import { userRouter } from "./users/users.js";
-import helmet from "helmet";
+// import helmet from "helmet";
 
 const port = 8000;
 const app = express();
 
 app.get("/hello", (req, res) => {
-  res.send("Hello");
+  // res.send("Hello");
+  throw new Error("Error!!!");
 });
 
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"], // Allow resources from the same origin
-      connectSrc: ["'self'", "http://localhost:8000"], // Allow connections to your API
-      // Add other directives as needed
-    },
-  })
-);
+app.use((req, res, next) => {
+  console.log("Time ", Date.now());
+  next();
+});
+
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"], // Allow resources from the same origin
+//       connectSrc: ["'self'", "http://localhost:8000"], // Allow connections to your API
+//       // Add other directives as needed
+//     },
+//   })
+// );
 
 app.use("/users", userRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(401).send(err.message);
+});
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
