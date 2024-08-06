c57cb4650cd5cb2f84599378d841f1e0d18d3592
Oleg Degterov <olegdegterov@gmail.com>
2024-08-06 18:01:29 +0300

    features: add user response

--- index.js
+++ index.js
@@ -3,34 +3,19 @@ import express from "express";
 const port = 8000;
 const app = express();
 
-app.all("/hello", (req, res, next) => {
-  console.log("all");
-  next();
+app.get("/hello", (req, res) => {
+  // res.cookie("token", "token123456", {
+  //   domain: "",
+  //   path: "/",
+  //   secure: true,
+  //   expires: 600000,
+  // });
+  // res.clearCookie("token");
+  // res.send("{ a: 1 }");
+  // if you no logic in cb function
+  // you must to call end() method
+  res.end();
 });
-
-const cb = (req, res, next) => {
-  console.log("cb");
-  next();
-};
-// you can use regExp in routes "/hello"
-// for example /.*a$/
-app.get("/hello", cb, (req, res) => {
-  res.send("Hello");
-});
-// you can use array cb1, cb2, cb3, for example
-// app.get("/hello", [cb1, cb2, cb3, (req, res) => {
-//   res.send("Hello");
-// }]);
-
-// you can use for route: user/hello if you want
-// do crud operations  for user
-// app.route("/user")
-//   .get("/hello", (req, res) => {
-//     res.send("Hello");
-//   })
-//   .post("/hello", (req, res) => {
-//     res.send("Hello");
-//   });
 
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
