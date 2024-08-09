d46dd1370c3c122898060fa1215509bffbaa124d
Oleg Degterov <olegdegterov@gmail.com>
2024-08-09 23:50:10 +0300

    features: add data validation

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from "class-validator";
+
 export class UserRegisterDto {
+  @IsEmail({}, { message: "Wrong email" })
   email: string;
+
+  @IsString({ message: "You can fill the password" })
   password: string;
+
+  @IsString({ message: "You can fill the name" })
   name: string;
 }
