36eb234c4d8f1996517d2771d7bcd48059c4eb16
Oleg Degterov <olegdegterov@gmail.com>
2024-08-09 11:50:16 +0300

    features: add user controller

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,30 @@
 import express, { Express } from "express";
-import { userRouter } from "./users/users";
 import { Server } from "http";
+import { LoggerService } from "./logger/logger.service";
+import { UserController } from "./users/users.controller";
 
 export class App {
   app: Express;
   server: Server;
   port: number;
+  logger: LoggerService;
+  userController: UserController;
 
-  constructor() {
+  constructor(logger: LoggerService, userController: UserController) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
+    this.userController = userController;
   }
 
   useRoutes() {
-    this.app.use("/users", userRouter);
+    this.app.use("/users", this.userController.router);
   }
+
   public async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port);
-    console.log(`Server started on http://localhost:${this.port}`);
+    this.logger.log(`Server started on http://localhost:${this.port}`);
+    // console.log(`Server started on http://localhost:${this.port}`);
   }
 }
