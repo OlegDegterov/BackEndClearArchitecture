77558e0b9d670020f201e606c60bcf8d8b24a59e
Oleg Degterov <olegdegterov@gmail.com>
2024-08-09 22:44:08 +0300

    features: add user entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import "reflect-metadata";
 import { IUserController } from "./users.controller.interface";
 import { UserLoginDto } from "./dto/user-login.dto";
 import { UserRegisterDto } from "./dto/user-register.dto";
+import { User } from "./user.entity";
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -23,12 +24,13 @@ export class UserController extends BaseController implements IUserController {
     next(new HTTPError(401, "Authorization error", "login"));
   }
 
-  register(
-    req: Request<{}, {}, UserRegisterDto>,
+  async register(
+    { body }: Request<{}, {}, UserRegisterDto>,
     res: Response,
     next: NextFunction
-  ) {
-    console.log(req.body);
-    this.ok(res, "register");
+  ): Promise<void> {
+    const newUser = new User(body.email, body.name);
+    await newUser.setPassword(body.password);
+    this.ok(res, newUser);
   }
 }
