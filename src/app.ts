dfb45b29373debb0c00674bfe821e6cbdc01e837
Oleg Degterov <olegdegterov@gmail.com>
2024-08-09 12:32:56 +0300

    features: add exception filter

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { Server } from "http";
 import { LoggerService } from "./logger/logger.service";
 import { UserController } from "./users/users.controller";
+import { ExceptionFilter } from "./errors/exception.filter";
 
 export class App {
   app: Express;
@@ -9,20 +10,32 @@ export class App {
   port: number;
   logger: LoggerService;
   userController: UserController;
+  exceptionFilter: ExceptionFilter;
 
-  constructor(logger: LoggerService, userController: UserController) {
+  constructor(
+    logger: LoggerService,
+    userController: UserController,
+    exceptionFilter: ExceptionFilter
+  ) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
     this.userController = userController;
+    this.exceptionFilter = exceptionFilter;
   }
 
   useRoutes() {
     this.app.use("/users", this.userController.router);
   }
 
+  useExceptionFilter() {
+    // чтобы не потерять контекст нужно забиндить на exceptionFilter
+    this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter));
+  }
+
   public async init() {
     this.useRoutes();
+    this.useExceptionFilter();
     this.server = this.app.listen(this.port);
     this.logger.log(`Server started on http://localhost:${this.port}`);
     // console.log(`Server started on http://localhost:${this.port}`);
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { BaseController } from "../common/base.controller";
 import { Request, Response, NextFunction } from "express";
 import { LoggerService } from "../logger/logger.service";
+import { HTTPError } from "../errors/http-error.class";
 
 export class UserController extends BaseController {
   constructor(logger: LoggerService) {
@@ -11,10 +12,11 @@ export class UserController extends BaseController {
     ]);
   }
   login(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, "login");
+    // this.ok(res, "login");
+    next(new HTTPError(401, "Authorization error", "login"));
   }
 
   register(req: Request, res: Response, next: NextFunction) {
-    this.ok(res, "register");
+    // this.ok(res, "register");
   }
 }
