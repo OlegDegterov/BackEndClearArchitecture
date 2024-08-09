33fa3766ba1614d331c08aefdeb768c1fa4820e5
Oleg Degterov <olegdegterov@gmail.com>
2024-08-09 23:17:00 +0300

    features: add user service

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,14 @@ import { ILogger } from "./logger/logger.interface";
 import { TYPES } from "./types";
 import { IExceptionFilter } from "./errors/exception.filter.interface";
 import { IUserController } from "./users/users.controller.interface";
+import { UserService } from "./users/user.service";
+import { IUserService } from "./users/user.service.interface";
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
   bind<ILogger>(TYPES.ILogger).to(LoggerService);
   bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
   bind<IUserController>(TYPES.UserController).to(UserController);
+  bind<IUserService>(TYPES.UserService).to(UserService);
   bind<App>(TYPES.Application).to(App);
 });
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,10 +9,15 @@ import { IUserController } from "./users.controller.interface";
 import { UserLoginDto } from "./dto/user-login.dto";
 import { UserRegisterDto } from "./dto/user-register.dto";
 import { User } from "./user.entity";
+import { UserService } from "./user.service";
+import { ILogger } from "../logger/logger.interface";
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
-  constructor(@inject(TYPES.ILogger) private loggerService: LoggerService) {
+  constructor(
+    @inject(TYPES.ILogger) private loggerService: ILogger,
+    @inject(TYPES.UserService) private userService: UserService
+  ) {
     super(loggerService);
     this.bindRoutes([
       { path: "/register", method: "post", func: this.register },
@@ -29,8 +34,10 @@ export class UserController extends BaseController implements IUserController {
     res: Response,
     next: NextFunction
   ): Promise<void> {
-    const newUser = new User(body.email, body.name);
-    await newUser.setPassword(body.password);
-    this.ok(res, newUser);
+    const result = await this.userService.createUser(body);
+    if (!result) {
+      return next(new HTTPError(422, "That user already exist"));
+    }
+    this.ok(res, result);
   }
 }
