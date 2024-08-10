fbd8b8e1eed268c976ab4cf9883046503091c2c2
Oleg Degterov <olegdegterov@gmail.com>
2024-08-10 18:34:51 +0300

    features: add nodemon eslint prettier

--- src/common/middleware.interface.ts
+++ src/common/middleware.interface.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express';
 export interface IMiddleWare {
-  execute: (req: Request, res: Response, next: NextFunction) => void;
+	execute: (req: Request, res: Response, next: NextFunction) => void;
 }
--- src/common/route.interface.ts
+++ src/common/route.interface.ts
@@ -1,9 +1,11 @@
-import { Request, Response, NextFunction, Router } from "express";
-import { IMiddleWare } from "./middleware.interface";
+import { Request, Response, NextFunction, Router } from 'express';
+import { IMiddleWare } from './middleware.interface';
 
 export interface IRoute {
-  path: string;
-  func: (req: Request, res: Response, next: NextFunction) => void;
-  method: keyof Pick<Router, "get" | "post" | "delete" | "patch" | "put">;
-  middlewares?: IMiddleWare[];
+	path: string;
+	func: (req: Request, res: Response, next: NextFunction) => void;
+	method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
+	middlewares?: IMiddleWare[];
 }
+
+export type ExpressReturnType = Response<any, Record<string, any>>;
